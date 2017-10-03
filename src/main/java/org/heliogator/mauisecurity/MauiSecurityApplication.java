255a7a51dfd041f68058dbf3741ce471b176891b
Gene <gk.heptonstall@gmail.com>
2017-10-03 16:10:04 +0800

    amend author

---
+++ src/main/java/org/heliogator/mauisecurity/MauiSecurityApplication.java
@@ -0,0 +1,12 @@
+package org.heliogator.mauisecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MauiSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MauiSecurityApplication.class, args);
+	}
+}
